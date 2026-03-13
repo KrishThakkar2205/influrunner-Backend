a7aeec06fd58548fe99dc812ae824ecadbf5a25f
KrishThakkar2205 <kdthakkar22@gmail.com>
2026-03-13 17:01:22 +0530

    Added Access token

--- backgound_workers/whatsapp_shoot_upload_reaminder.py
+++ backgound_workers/whatsapp_shoot_upload_reaminder.py
@@ -12,8 +12,8 @@ def send_shoot_upload_reminder():
             "Authorization": "Bearer EAANLSkTxaEgBQZC6a5tIBZCzN3uQoNCenjzBHctbaI09IuNkwi9ULEPhN7Q5D9eMhXvTvCznXv4cesZBZA7FN6RNZBdZCsRchd2NwsJ4oz8byaDXeFDb6RxrSeaPi6R3iqQ7R9rJfVSuAPSknE7WSAYUBGCmxDUMF8vfgSP2quLa6jlqaMT3Yr3a3BzCNKaTeArkTBvx5yVshf2pTo7SyRpo2f9nVQapDIEA5Mi59aZC0cRl0GW4PwlUcV2AZAjHEGgFZA571kbPjzombLQxbluWPCaZB2Vs0MS4z1BX4e8wZDZD"
         }
         now = datetime.utcnow().date()
-        shoots = db.query(Shoots).filter(Shoots.shoot_date == now).first()
-        if shoots:
+        shoot = db.query(Shoots).filter(Shoots.shoot_date == now).first()
+        if shoot:
             payload = {
                     "messaging_product": "whatsapp",
                     "recipient_type": "individual",

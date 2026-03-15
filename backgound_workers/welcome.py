f043cba7dc67a3862b961cb1af69edeb6cce3dbc
KrishThakkar2205 <kdthakkar22@gmail.com>
2026-03-16 00:13:09 +0530

    Added New Condition to avoid error

--- backgound_workers/welcome.py
+++ backgound_workers/welcome.py
@@ -14,7 +14,8 @@ def connect_instagram():
         influencer_ids = [row[0] for row in result]
         for influencer_id in influencer_ids:
             device_token = db.query(DeviceTokens.device_token).filter(DeviceTokens.influencer_id == influencer_id).first()
-            send_notification(device_token.device_token, "⚡ Don’t Leave Your Profile Incomplete", "Connect Instagram to unlock your full creator portfolio.")
+            if device_token:
+                send_notification(device_token.device_token, "⚡ Don’t Leave Your Profile Incomplete", "Connect Instagram to unlock your full creator portfolio.")
     except Exception as e:
         print(e)
     finally:

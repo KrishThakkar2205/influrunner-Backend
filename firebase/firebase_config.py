94168b60b319d3860b3acb3cc76640e0b0bcbf38
KrishThakkar2205 <kdthakkar22@gmail.com>
2026-03-15 23:29:10 +0530

    Added new file

--- firebase/firebase_config.py
+++ firebase/firebase_config.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("firebase/influrunner-78978-firebase-adminsdk-fbsvc-d1cb84dc2e.json")
+cred = credentials.Certificate("firebase/influrunner-78978-firebase-adminsdk-fbsvc-393d319de2.json")
 
 firebase_admin.initialize_app(cred)

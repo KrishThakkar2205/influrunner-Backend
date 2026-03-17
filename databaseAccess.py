a5570f9912f07b13c3c3b92732df56f9a5543369
KrishThakkar2205 <kdthakkar22@gmail.com>
2026-03-17 11:06:38 +0530

    Solving Error

--- databaseAccess.py
+++ databaseAccess.py
@@ -61,7 +61,8 @@ def FinalSignup(db: Session, email_id: str, min_price: int, max_price: int, cate
 
 def Login(db: Session, email_id: str, password: str):
     influencer = db.query(Influencer).filter(Influencer.email_id == email_id).first()
-    print(len(influencer.password_hash))
+    print(len(str(influencer.password_hash)))
+    print(str(influencer.password_hash))
     if influencer and bcrypt.checkpw(password.encode('utf-8'), influencer.password_hash.encode('utf-8')):
         return {
             "id":influencer.id,

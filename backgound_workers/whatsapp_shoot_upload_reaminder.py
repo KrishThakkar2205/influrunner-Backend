fb1f68bf40765f6126dd6c300e48c12d634af461
KrishThakkar2205 <kdthakkar22@gmail.com>
2026-06-04 17:35:15 +0530

    Added 1 Hr Notification

--- backgound_workers/whatsapp_shoot_upload_reaminder.py
+++ backgound_workers/whatsapp_shoot_upload_reaminder.py
@@ -142,15 +142,78 @@ def send_shoot_reminder_bfr_1hr():
             ist_time = shoot_date_time_utc.astimezone(ZoneInfo("Asia/Kolkata"))
             formatted_time = ist_time.strftime("%I:%M %p")
             if timedelta(minutes=55) <= diff <= timedelta(hours=1):
-                infleuncer = db.query(Influencer.name, Influencer.phone_number).filter(Influencer.id == shoot.influencer_id).first()
-                device_token = db.query(DeviceTokens.device_token).filter(DeviceTokens.influencer_id == shoot.influencer_id).all()
-                for token in device_token:
-                    try:
-                        status = send_notification(token.device_token, "Shoot Remainder Before 1 hour", f"Brand Name : {shoot.brand_name}\nShoot Time : {formatted_time}\nLocation : {shoot.location}\n\nNotes : {shoot.notes}\n\nBe On time")
-                        if status != 200:
-                            print(f"Failed to send shoot upload reminder for shoot {shoot.id}")
-                    except Exception as e:
-                        print(e)
+                infleuncer_details = db.query(Influencer.name, Influencer.phone_number).filter(
+                    Influencer.id == shoot.influencer_id
+                ).first()
+                
+                if not infleuncer_details:
+                    print(f"[1HR] ❌ Influencer not found for shoot ID {shoot.id} with influencer_id {shoot.influencer_id}")
+                    continue
+                
+                try:
+                    payload_for_shoot_remainder = {
+                        "messaging_product": "whatsapp",
+                        "to": infleuncer_details.phone_number,
+                        "type": "template",
+                        "template": {
+                            "name": "shoot_remainder",
+                            "language": {
+                                "code": "en"
+                            },
+                            "components": [
+                                {
+                                    "type" : "header",
+                                    "parameters": [
+                                        {
+                                            "type": "text",
+                                            "parameter_name": "hour",
+                                            "text": "1 Hours"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "body",
+                                    "parameters": [
+                                        {
+                                            "type": "text",
+                                            "parameter_name": "name",
+                                            "text": infleuncer_details.name
+                                        },{
+                                            "type": "text",
+                                            "parameter_name": "brand_name",
+                                            "text": shoot.brand_name
+                                        },{
+                                            "type": "text",
+                                            "parameter_name": "shoot_date",
+                                            "text": shoot_date_time.strftime("%d %B %Y")
+                                        },{
+                                            "type": "text",
+                                            "parameter_name": "shoot_time",
+                                            "text": formatted_time
+                                        },{
+                                            "type": "text",
+                                            "parameter_name": "notes",
+                                            "text": shoot.notes
+                                        },{
+                                            "type":"text",
+                                            "parameter_name":"location",
+                                            "text":shoot.location
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    }
+
+                    response = requests.post(
+                    url,
+                    headers=headers,
+                    json=payload_for_shoot_remainder
+                    )
+                
+                    print(response.json())
+                except Exception as e:
+                    print(e)
                 shoot.notify_before_1hr = True
                 db.commit()
     except Exception as e:

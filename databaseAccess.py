1a0053df8986b1a5ef11bafb28de1576d87e7df2
KrishThakkar2205 <kdthakkar22@gmail.com>
2026-06-14 12:16:02 +0530

    Added Tryiing to figure thingd out got Engaged Audience Demograpics

--- databaseAccess.py
+++ databaseAccess.py
@@ -560,31 +560,31 @@ def GetInstaPortfolioMetric(db: Session, infleuncer_id: str):
     for item in data['data']:
         response_to_browser[item['name']] = item['total_value']['value']
 
-    url = f"https://graph.instagram.com/v25.0/{id}/insights?metric=engaged_audience_demographics&period=lifetime&timeframe=this_month&breakdown=city&metric_type=total_value&access_token={credentials.access_token}"
+    url = f"https://graph.instagram.com/v25.0/{id}/insights?metric=engaged_audience_demographics&period=lifetime&timeframe=this_month&metric_type=total_value&access_token={credentials.access_token}"
     response = requests.get(url)
     data = response.json()
-    results = (
-        data["data"][0]
-        ["total_value"]["breakdowns"][0]
-        ["results"]
-    )
+    # results = (
+    #     data["data"][0]
+    #     ["total_value"]["breakdowns"][0]
+    #     ["results"]
+    # )
 
-    top_cities = sorted(
-        results,
-        key=lambda x: x["value"],
-        reverse=True
-    )[:5]
+    # top_cities = sorted(
+    #     results,
+    #     key=lambda x: x["value"],
+    #     reverse=True
+    # )[:5]
 
-    formatted = [
-        {
-            "city": item["dimension_values"][0],
-            "engagement": item["value"]
-        }
-        for item in top_cities
-    ]
+    # formatted = [
+    #     {
+    #         "city": item["dimension_values"][0],
+    #         "engagement": item["value"]
+    #     }
+    #     for item in top_cities
+    # ]
     print(data)
-    print("Formatted")
-    print(formatted)
+    # print("Formatted")
+    # print(formatted)
     response_to_browser["city"] = formatted
     return response_to_browser
 

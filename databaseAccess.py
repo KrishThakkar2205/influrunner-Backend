070f8b69c0e01b34498210f078975f8278281115
KrishThakkar2205 <kdthakkar22@gmail.com>
2026-08-31 17:24:44 +0530

    added new data for testing

--- databaseAccess.py
+++ databaseAccess.py
@@ -524,7 +524,7 @@ def GetInstaPortfolioMetric(db: Session, infleuncer_id: str):
     url = f"https://graph.instagram.com/v25.0/me?fields=username,media_count,followers_count&access_token={credentials.access_token}"
     response = requests.get(url)
     data = response.json()
-    # print(data)
+    print(data)
     response_to_browser["username"] = data["username"]
     response_to_browser["media_count"] = data["media_count"]
     response_to_browser["followers_count"] = data["followers_count"]
@@ -538,7 +538,7 @@ def GetInstaPortfolioMetric(db: Session, infleuncer_id: str):
     url = f"https://graph.instagram.com/v25.0/{id}/insights?metric={metrices}&period=day&since={since}&until={until}&metric_type=total_value&access_token={credentials.access_token}"
     response = requests.get(url)
     data = response.json()
-    # print(data)
+    print(data)
     for item in data['data']:
         response_to_browser[item['name']] = item['total_value']['value']
 
@@ -570,6 +570,7 @@ def GetInstaPortfolioMetric(db: Session, infleuncer_id: str):
     url = f"https://graph.instagram.com/v25.0/{id}/insights?metric=engaged_audience_demographics&period=lifetime&timeframe=this_month&breakdown=age&metric_type=total_value&access_token={credentials.access_token}"
     response = requests.get(url)
     data = response.json()
+    print(data)
     breakdown = data["data"][0]["total_value"]["breakdowns"][0]["results"]
     total = sum(item["value"] for item in breakdown)
     formatted = [
